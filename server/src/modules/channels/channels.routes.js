f5f61af1fe5c0b97f1c858afb04e6fbc2e0b6e0e
alu0101513379 <alu0101513379@ull.edu.es>
2026-05-09 22:26:19 +0100

    feat: add route to mark channel as read

--- server/src/modules/channels/channels.routes.js
+++ server/src/modules/channels/channels.routes.js
@@ -4,6 +4,7 @@ const authenticate = require('../../middleware/authenticate');
 const validate = require('../../middleware/validate');
 const { createChannelSchema } = require('./channels.schema');
 const ctrl = require('./channels.controller');
+const messagesCtrl = require('../messages/messages.controller');
 
 const serversRouter = Router({ mergeParams: true });
 serversRouter.use(authenticate);
@@ -12,5 +13,6 @@ serversRouter.post('/', validate(createChannelSchema), ctrl.createChannel);
 const channelsRouter = Router();
 channelsRouter.use(authenticate);
 channelsRouter.delete('/:id', ctrl.deleteChannel);
+channelsRouter.patch('/:id/read', messagesCtrl.markChannelRead);
 
 module.exports = { serversRouter, channelsRouter };
